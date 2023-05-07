525c315172cf07d69452c4d2679a79182f0680b0
aoi562227 <aoi562227@gmail.com>
2023-05-07 10:08:09 +0900

    sql jpa추가 데이터베이스 생성기능

--- imageedit/src/main/java/capstone/imageedit/login/domain/member/Member.java
+++ imageedit/src/main/java/capstone/imageedit/login/domain/member/Member.java
@@ -1,22 +1,32 @@
 package capstone.imageedit.login.domain.member;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 
 @Data
+@Entity
+@Getter @Setter
 public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     @NotEmpty
     private String loginId;
 
     @NotEmpty
-
     private String name;
 
     @NotEmpty
-
     private String password;
 
 
